d885ece5151171620420b9535eb609f36bd6907c
Ismael Adriano <ismael_adriano@hotmail.com>
2021-03-08 11:27:03 -0300

    Ajustes possibles names blocks

--- src/PAFNFCeBuilder.php
+++ src/PAFNFCeBuilder.php
@@ -17,15 +17,15 @@ use IsmaelAdriano\PAFNFCe\PAFNFCe;
 final class PAFNFCeBuilder extends PAFNFCe
 {
     protected $possibles = [
-        'blockU1' => ['class' => BlockU1::class, 'order' => 1],
-        'blockA2' => ['class' => BlockA2::class, 'order' => 2],
-        'blockP2' => ['class' => BlockP2::class, 'order' => 3],
-        'blockE2' => ['class' => BlockE2::class, 'order' => 4],
-        'blockD2' => ['class' => BlockD2::class, 'order' => 5],
-        'blockD3' => ['class' => BlockD3::class, 'order' => 6],
-        'blockD4' => ['class' => BlockD4::class, 'order' => 7],
-        'blockJ1' => ['class' => BlockJ1::class, 'order' => 8],
-        'blockJ2' => ['class' => BlockJ2::class, 'order' => 9],
-        'blockEAD' => ['class' => BlockEAD::class, 'order' => 10],
+        'blocku1' => ['class' => BlockU1::class, 'order' => 1],
+        'blocka2' => ['class' => BlockA2::class, 'order' => 2],
+        'blockp2' => ['class' => BlockP2::class, 'order' => 3],
+        'blocke2' => ['class' => BlockE2::class, 'order' => 4],
+        'blockd2' => ['class' => BlockD2::class, 'order' => 5],
+        'blockd3' => ['class' => BlockD3::class, 'order' => 6],
+        'blockd4' => ['class' => BlockD4::class, 'order' => 7],
+        'blockj1' => ['class' => BlockJ1::class, 'order' => 8],
+        'blockj2' => ['class' => BlockJ2::class, 'order' => 9],
+        'blockead' => ['class' => BlockEAD::class, 'order' => 10],
     ];
 }
