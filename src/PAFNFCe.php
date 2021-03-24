c12edc5a4aebd7f56f7a7996880715972816d092
Ismael Adriano <ismael_adriano@hotmail.com>
2021-03-24 14:20:41 -0300

    Ajuste geração chave EAD

--- src/PAFNFCe.php
+++ src/PAFNFCe.php
@@ -26,8 +26,36 @@ abstract class PAFNFCe
         foreach ($keys as $key) {
             if (isset($this->$key)) {
                 $pafNFce .= $this->$key;
+
             }
         }
+
+        $EADGenarator = $this->generateEAD($pafNFce);
+        $pafNFce .= 'EAD'.strtoupper($EADGenarator);
+
         return $pafNFce;
+    }
+
+    private function generateEAD($pafNFce)
+    {
+        $md5FileContent = md5($pafNFce);
+        $signature = [];
+        $new_key_pair = openssl_pkey_new(array(
+            "private_key_bits" => 1024,
+            "private_key_type" => OPENSSL_KEYTYPE_RSA,
+        ));
+
+        openssl_pkey_export($new_key_pair, $private_key_pem);
+
+        $details = openssl_pkey_get_details($new_key_pair);
+        $public_key_pem = $details['key'];
+
+        openssl_sign($md5FileContent, $signature, $private_key_pem, OPENSSL_ALGO_SHA1);
+
+        if (openssl_verify($md5FileContent, $signature, $public_key_pem, "RSA-SHA256")) {
+            throw new \Exception("Não foi possível assinar o arquivo PAF NFC-e");
+        }
+
+        return bin2hex($details['rsa']['n']);
     }
 }
